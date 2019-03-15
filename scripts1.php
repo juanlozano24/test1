84687d9597b11e021d05203f8c4ad62428fc14c7
juanlozano24 <jandresl092@gmail.com>
2019-03-14 21:54:45 -0500

    scripts

---
+++ scripts1.php
@@ -0,0 +1,11 @@
+<?php
+    //mi primer script en PHP
+    //DESAROLLADO POR JUAN LOZANO
+    $nombre = "juan";
+    $apellido = "lozano";
+    $email = "jandresl092@gmail.com";
+    
+    echo $nombre;
+    echo $apellido;
+    echo $email;
+?>
---
+++ scripts_ejemplos/scripts1.php
@@ -0,0 +1,18 @@
+
+<center>MI PRIMER SCRIPT</center>
+<?php
+    //mi primer script en PHP
+    //DESAROLLADO POR JUAN LOZANO
+    $nombre = "juan";
+    $apellido = "lozano";
+    $email = "jandresl092@gmail.com";
+    
+    echo "<center>HTML EN PHP</center>";
+    echo "<center>";
+    echo "Bienvenidos: ".$nombre." ".$apellido."<br>";
+    echo "</center>";
+    echo $nombre."<br>";
+    echo $apellido."<br>";
+    echo $email."<br>";
+?>
+<center>FIN SCRIPT</center>
